916b5b9715ec663d9ae26b7d68961728fc05c33e
Paul Morris <pmorris2012@fau.edu>
2022-03-10 21:52:53 -0500

    wrappers

---
+++ jaks/modules/wrapper.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+from dataclasses import dataclass
+
+import jax
+import jax.numpy as jnp
+
+from .core import Module
+
+
+@dataclass
+class Residual(Module):
+    module: Module
+
+    def modules(self):
+        yield "residual_module", self.module
+
+    def forward(self, params: OrderedDict, x: jnp.ndarray) -> jnp.ndarray:
+        return self.residual_module(params, x) + x
+
+
+@dataclass
+class Loop(Module):
+    module: Module
+    iterations: int
+
+    def modules(self):
+        yield "loop_module", self.module
+
+    def forward(self, params: OrderedDict, x: jnp.ndarray) -> jnp.ndarray:
+        def step_fn(i, x):
+            return self.loop_module(params, x)
+        return jax.lax.fori_loop(0, self.iterations, step_fn, x)
