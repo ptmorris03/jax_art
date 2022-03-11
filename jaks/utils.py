f3a7e6b617a83020d00e19ae901306c051df7e1f
Paul Morris <pmorris2012@fau.edu>
2022-03-10 23:50:34 -0500

    loss and accuracy

--- jaks/utils.py
+++ jaks/utils.py
@@ -1,4 +1,5 @@
 import jax
+import jax.numpy as jnp
 
 from collections import OrderedDict
 import json
@@ -10,3 +11,13 @@ def pretty_params(params: OrderedDict, indent: int = 4) -> str:
         indent = indent,
         sort_keys = False,
     )
+
+def log_loss(predictions: jnp.ndarray, labels: jnp.ndarray) -> jnp.ndarray:
+    lse = jax.scipy.special.logsumexp(predictions, axis=-1, keepdims=True)
+    log_predictions = predictions - lse
+    target_distribution = jax.nn.one_hot(labels, preds.shape[-1])
+    return -jnp.mean(log_predictions * target_distribution)
+
+
+def accuracy(predictions: jnp.ndarray, labels: jnp.ndarray) -> jnp.ndarray:
+    return predictions.argmax(axis=-1) == labels
