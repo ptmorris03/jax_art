9d561f42c22929db550791297b9365a97dca66af
Paul Morris <pmorris2012@fau.edu>
2022-03-10 22:33:08 -0500

    grouped transforms

--- jaks/modules/transform.py
+++ jaks/modules/transform.py
@@ -41,3 +41,17 @@ class DotProduct(Module):
 
     def forward(self, params: OrderedDict, x: jnp.ndarray) -> jnp.ndarray:
         return jnp.dot(x, params["vector"])
+
+
+@dataclass
+class Rotate(Module):
+    in_dims: int
+    out_dims: int
+    init_scale: float = 1e-2
+
+    def parameters(self, random_key: PRNGKey):
+        matrix = jax.random.normal(random_key, (self.out_dims, self.in_dims))
+        yield "matrix", matrix * self.init_scale
+
+    def forward(self, params: OrderedDict, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.matmul(params["matrix"], x)
