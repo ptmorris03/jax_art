05ba795cf6bc3d13dc4ac52ee9712699fa809793
Paul Morris <pmorris2012@fau.edu>
2022-03-11 00:40:29 -0500

    package

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='jaks',
+    version='1',
+    author='Paul Morris',
+    author_email='pmorris2012@fau.edu',
+    description='jaks',
+    long_description='jaks',
+    long_description_content_type="text/markdown",
+    url='https://github.com/ptmorris03/jax_workbench',
+    project_urls = {
+        "Bug Tracker": "https://github.com/ptmorris03/jax_workbench/issues"
+    },
+    license='to kill',
+    packages=['jaks'],
+    install_requires=[],
+)
