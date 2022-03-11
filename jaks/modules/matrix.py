03947bbaae4b8e4e6930878dc3bb1662537a1b5d
Paul Morris <pmorris2012@fau.edu>
2022-03-10 21:44:23 -0500

    vector and matrix ops

--- jaks/modules/matrix.py
+++ jaks/modules/matrix.py
@@ -1,7 +1,23 @@
+from collections import OrderedDict
+from dataclasses import dataclass
+from typing import Optional, Union, Tuple, Callable, Any, Iterable
+
 import jax
 import jax.numpy as jnp
 from jax.random import PRNGKey
 
-from collections import OrderedDict
-from dataclasses import dataclass
-from typing import Optional, Union, Tuple, Callable, Any, Iterable
+from .core import Module
+
+
+@dataclass
+class Rotate(Module):
+    in_dims: int
+    out_dims: int
+    init_scale: float = 1e-2
+
+    def parameters(self, random_key: PRNGKey):
+        matrix = jax.random.normal(random_key, (self.out_dims, self.in_dims))
+        yield "matrix", matrix * self.init_scale
+
+    def forward(self, params: OrderedDict, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.matmul(params["matrix"], x)
--- jaks/modules/vector.py
+++ jaks/modules/vector.py
@@ -1,14 +1,12 @@
-import jax
-import jax.numpy as jnp
-from jax.random import PRNGKey
-
 from collections import OrderedDict
 from dataclasses import dataclass
 from typing import Optional, Union, Tuple, Callable, Any, Iterable
 
-#####        #####
-#VECTOR FUNCTIONS#
-#####        #####
+import jax
+import jax.numpy as jnp
+from jax.random import PRNGKey
+
+from .core import Module
 
 
 @dataclass
