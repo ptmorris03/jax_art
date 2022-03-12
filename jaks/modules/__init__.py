85474a635b0f10c86ba090af541cbbdf75e32005
Paul Morris <pmorris2012@fau.edu>
2022-03-11 23:25:40 -0500

    ResNetStack

--- jaks/modules/__init__.py
+++ jaks/modules/__init__.py
@@ -1,5 +1,5 @@
 from .core import Module
 from .transform import Translate, Scale, DotProduct, Rotate
 from .nonlin import RELU, QuickGELU, ZScore
-from .wrapper import Residual, ResidualStack, Loop
+from .wrapper import Residual, ResNetStack, Loop
 from .layer import Linear, LayerNorm, MLP
