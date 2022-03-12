3f447d641399fe63bb1a4d6d211f59f0d193d493
Paul Morris <pmorris2012@fau.edu>
2022-03-11 23:18:55 -0500

    imports

--- jaks/modules/__init__.py
+++ jaks/modules/__init__.py
@@ -1,5 +1,5 @@
 from .core import Module
 from .transform import Translate, Scale, DotProduct, Rotate
 from .nonlin import RELU, QuickGELU, ZScore
-from .wrapper import Residual, Loop
+from .wrapper import Residual, ResidualStack, Loop
 from .layer import Linear, LayerNorm, MLP
