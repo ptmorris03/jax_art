18fdcf2b64a7db5bf899c6ebda357da08ea9768a
Paul Morris <pmorris2012@fau.edu>
2022-03-11 22:26:49 -0500

    typo

--- jaks/modules/__init__.py
+++ jaks/modules/__init__.py
@@ -1,5 +1,5 @@
 from .core import Module
-from .transform import Translace, Scale, DotProduct, Rotate
+from .transform import Translate, Scale, DotProduct, Rotate
 from .nonlin import RELU, QuickGELU, ZScore
 from .wrapper import Residual, Loop
 from .layer import Linear, LayerNorm, MLP
