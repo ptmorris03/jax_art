1a6abf1a7169e688c473586c6ce4e9f1afeae689
Paul Morris <pmorris2012@fau.edu>
2022-03-12 00:43:39 -0500

    name change

--- jaks/utils.py
+++ jaks/utils.py
@@ -12,7 +12,7 @@ def pretty_params(params: OrderedDict, indent: int = 4) -> str:
 def log_loss(predictions: jnp.ndarray, labels: jnp.ndarray) -> jnp.ndarray:
     lse = jax.scipy.special.logsumexp(predictions, axis=-1, keepdims=True)
     log_predictions = predictions - lse
-    target_distribution = jax.nn.one_hot(labels, preds.shape[-1])
+    target_distribution = jax.nn.one_hot(labels, predictions.shape[-1])
     return -jnp.mean(log_predictions * target_distribution)
 
 def accuracy(predictions: jnp.ndarray, labels: jnp.ndarray) -> jnp.ndarray:
