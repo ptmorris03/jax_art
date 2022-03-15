c3e1acd42b3d6e7135ac9b47bbfde7846f0f027e
Paul Morris <pmorris2012@fau.edu>
2022-03-15 17:17:54 -0400

    title

--- experiments/outer_product/image_patches/train.py
+++ experiments/outer_product/image_patches/train.py
@@ -113,7 +113,7 @@ def plot_compare(loss1, acc1, loss2, acc2, res=1440, dpi=120, title=""):
 
     inches = res / dpi
     plt.gcf().set_size_inches(inches * 16 / 9, inches)
-    plt.gcf().set_title(title)
+    plt.suptitle(title)
     plt.savefig("results.png", dpi=dpi)
 
 
